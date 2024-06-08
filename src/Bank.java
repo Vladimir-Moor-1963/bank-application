ede544e43a1e31bf9aaba951a487b86ce910ef7b
Vladimir <papa1963@hotmail.de>
2024-06-08 13:33:42 +0200

    init

---
+++ src/Bank.java
@@ -0,0 +1,53 @@
+import java.util.Objects;
+
+public class Bank {
+    private long id;
+    private String name;
+
+    public Bank(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Bank bank = (Bank) o;
+
+        if (id != bank.id) return false;
+        return Objects.equals(name, bank.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
